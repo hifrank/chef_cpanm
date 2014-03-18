e3dc56c92d71a825945e71a5227538ff22bdda18
Frank Chen <frank_chen@htc.com>
2014-03-19 00:23:36 +0800

    init commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'chef_cpanm'
+maintainer       'htc'
+maintainer_email 'frank_chen@htc.com'
+license          'All rights reserved'
+description      'Installs/Configures chef_cpanm'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: chef_cpanm
+# Recipe:: default
+#
+# Copyright 2014, htc
+#
+# All rights reserved - Do Not Redistribute
+#
