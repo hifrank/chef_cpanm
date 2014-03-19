cb193cf88ea7b3adb101833e60aae9fe53169d94
Frank Chen <frank_chen@htc.com>
2014-03-19 11:48:41 +0800

    rename to cpanm

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
-name             'chef_cpanm'
+name             'cpanm'
 maintainer       'htc'
 maintainer_email 'frank_chen@htc.com'
 license          'All rights reserved'
-description      'Installs/Configures chef_cpanm'
+description      'Installs/Configures cpanminus'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
