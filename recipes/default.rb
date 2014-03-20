5d9e3b85952122d56753920cb5906e479da41d3d
Frank Chen <frank_chen@htc.com>
2014-03-20 11:08:37 +0000

    add rpm file from http://pkgs.org/centos-5/flexbox-i386/perl-ExtUtils-MakeMaker-6.56-5.el5.noarch.rpm.html for centos5 modify cookbook to install by package on centos 5.

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,10 +7,31 @@
 # All rights reserved - Do Not Redistribute
 #
 if platform?('centos')
-  package "perl-ExtUtils-MakeMaker"
   # some cpam need gcc & openssl for compile & test.
   package "gcc"
   package "openssl-devel"
+  package "make"
+
+  if 6 == node['platform_version'].to_i
+    package "perl-ExtUtils-MakeMaker" 
+  else
+    cookbook_file "perl-ExtUtils-MakeMaker-6.56-5.el5.noarch.rpm" do
+      mode "0755"
+      owner "root"
+      group "root"
+      path "/tmp/perl-ExtUtils-MakeMaker-6.56-5.el5.noarch.rpm"
+      not_if "rpm -q perl-ExtUtils-MakeMaker"
+    end
+    bash "install perl-ExtUtils" do
+      user "root"
+      cwd  "/tmp/"
+      code <<-EOH
+        rpm -ivh /tmp/perl-ExtUtils-MakeMaker-6.56-5.el5.noarch.rpm
+      EOH
+      not_if "rpm -q perl-ExtUtils-MakeMaker"
+    end
+  end
+
 end
 
 cookbook_file "cpanm" do
@@ -21,3 +42,4 @@ cookbook_file "cpanm" do
   action :create_if_missing
   not_if { node['cpanm']['install_from'] == 'source' }
 end
+
